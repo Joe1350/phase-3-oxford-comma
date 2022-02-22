e954b320ec61d9a105d4e1d9426e0c284ce1e426
Joseph Martin <josephmartin1350@gmail.com>
2022-02-21 22:15:27 -0700

    commit tree bf869c37042df9031673cb7110e802b6b088e971

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.first
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length == 3
+        "#{array[0]}, #{array[1]}, and #{array[2]}"
+    else
+        array[-1] = "and #{array[-1]}"
+        array.join(", ")
+    end
 end
